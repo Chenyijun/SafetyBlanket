fa81ed2720482eb3eb55af750fecede6a8debc05
Annie Chen <annieche@andrew.cmu.edu>
2015-12-10 19:06:19 -0500

    Extension

---
+++ SafetyBlanket/Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  Extensions.swift
+//  SafetyBlanket
+//
+//  Created by Annie Chen on 12/10/15.
+//  Copyright © 2015 Annie Chen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func addBackground() {
+        // screen width and height:
+        let width = UIScreen.mainScreen().bounds.size.width
+        let height = UIScreen.mainScreen().bounds.size.height
+        
+        let imageViewBackground = UIImageView(frame: CGRectMake(0, 0, width, height))
+        imageViewBackground.image = UIImage(named: "blueBokeh")
+        
+        // you can change the content mode:
+        imageViewBackground.contentMode = UIViewContentMode.ScaleAspectFill
+        
+        self.addSubview(imageViewBackground)
+        self.sendSubviewToBack(imageViewBackground)
+    }}
